8172d923f947330b15e9456e1aa18ff3b62f80ab
Anthony Arinze <anthonyarinze28@gmail.com>
2025-01-18 15:46:44 +0100

    Initial commit for Grimoire project

---
+++ src/app/pages/home/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/app/ui/header";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
---
+++ src/app/ui/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="sticky h-14 flex flex-row items-center text-black font-bold bg-white shadow-lg">
+      <h1 className="text-2xl ml-4">Grimoire</h1>
+    </header>
+  );
+}
